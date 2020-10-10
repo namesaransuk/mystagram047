3b290deab59651c09b7b8a715ccd601ac50d8370
namesaransuk <49622873+namesaransuk@users.noreply.github.com>
2020-10-10 18:50:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-const indexRouter = require('./routes/index');
-const path = require('path');
-const morgan = require('morgan');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
-const PORT = process.env.PORT|| 8080;
+const PORT = process.env.PORT || 8080;
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const hostname = '127.0.0.1'
+const port = 3000;
 
-// 1. Configuration here
+// const handler = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/plain")
+//     res.end("Hello World")
+// })
 
+const handler = (req, res) => {
+    const filename = path.join(__dirname, 'index.html')
+    const indexData = fs.readFileSync(filename)
+    res.end(indexData)
+}
+const server = http.createServer(handler)
 
-
-const fetch = require('node-fetch');
-global.fetch = fetch;
-
-const app = express();
-
-//Middleware เพื่ออ่าน req.body
-app.use(express.json());
-app.use(express.urlencoded({extended:false}))
-
-//router
-app.use('/', indexRouter);
-
-//Middleware
-app.use(cors());
-app.use(morgan('common'));
-app.use(express.static(path.join(__dirname,'public')));
-
-app.listen(
-  PORT,
-  () => {
-    console.log(`Listening to port ${PORT}`);
-  }
+server.listen(
+    PORT,
+    () => {
+        console.log(`Listening to port ${PORT}`);
+    }
 );
